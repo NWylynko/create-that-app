41e77beb1ff9b213aa999590fb3c9722754e8439
Nick Wylynko <nick@wylynko.com>
2022-08-15 15:29:37 +0800

    Add in more options

--- src/initialisers.ts
+++ src/initialisers.ts
@@ -8,36 +8,55 @@ export const initialisers = ({ runner, manager }: Options): Initialiser[] => {
   return [
     {
       id: uuid(),
-      name: `React App`,
+      name: `React`,
       command: ({ name }: CommandOptions) => `${runner}react-app${latest} ${name}`,
       requiresName: true,
     },
     {
       id: uuid(),
-      name: `Typescript React App`,
+      name: `Typescript React`,
       command: ({ name }: CommandOptions) => `${runner}react-app${latest} ${name} --template typescript`,
       requiresName: true,
       typescript: true,
     },
     {
       id: uuid(),
-      name: `T3 App`,
+      name: `T3`,
       command: ({}: CommandOptions) => `${runner}t3-app${latest}`,
       requiresName: false,
       typescript: true,
     },
     {
       id: uuid(),
-      name: `Next App`,
+      name: `Next`,
       command: ({}: CommandOptions) => `${runner}next-app${latest}`,
       requiresName: false,
     },
     {
       id: uuid(),
-      name: `Typescript Next App`,
+      name: `Typescript Next`,
       command: ({}: CommandOptions) => `${runner}next-app${latest} --typescript`,
       requiresName: false,
       typescript: true,
-    }
+    },
+    {
+      id: uuid(),
+      name: `Gatsby`,
+      command: ({ name }: CommandOptions) => `${runner}gatsby${latest} ${name}`,
+      requiresName: true,
+    },
+    {
+      id: uuid(),
+      name: `Typescript Gatsby`,
+      command: ({ name }: CommandOptions) => `${runner}gatsby${latest} ${name} --ts`,
+      requiresName: true,
+      typescript: true,
+    },
+    {
+      id: uuid(),
+      name: `Vue`,
+      command: ({ name }: CommandOptions) => `${runner}vue-app${latest} ${name}`,
+      requiresName: true,
+    },
   ]
 }
